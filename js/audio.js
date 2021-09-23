c8993f9ebd46c30acba1f059166212f24dc6d621
TuanNguyen <estatic.fear2011@gmail.com>
2021-09-24 00:24:03 +0700

    update background, music

---
+++ js/audio.js
@@ -0,0 +1,10 @@
+var audio;
+export function play() {
+  if (!audio) {
+    audio = new Audio("../music/darling.mp3");
+    audio.loop = true;
+  }
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+}
